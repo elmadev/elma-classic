24b4778f5e80e65e718293d756a86b0f56d4e8f2
Joey Gouly <joey.gouly@gmail.com>
2025-12-07 15:47:29 +0000

    fix(build): add stub file
    This contains stubs for every 'platform' function that will be replaced with
    SDL functionality.

---
+++ stub.cpp
@@ -0,0 +1,60 @@
+#include "ALL.H"
+
+int main() {
+	return 0;
+}
+
+int Locky0_alul = 0;
+
+unsigned char** lockbackbuffer(int unused) {
+	return nullptr;
+}
+
+void unlockbackbuffer() { }
+
+unsigned char** lockfrontbuffer() {
+	return nullptr;
+}
+
+void unlockfrontbuffer() { }
+
+ddpal::ddpal(unsigned char* tomb) { }
+ddpal::~ddpal() { }
+void ddpal::set() { }
+
+void uzenet(char* text1, char* text2, char* text3) {
+	printf("%s %s %s\n", text1, text2, text3);
+}
+
+void hiba(char* text1, char* text2, char* text3) {
+	printf("%s %s %s\n", text1, text2, text3);
+}
+
+int s_random(int maximum) { return 0; }
+
+void mv_check(int unused) { }
+
+void setmou(int x, int y) { }
+void getmou(int* x, int* y) { }
+
+double mv_stopperido(void) { return 0; }
+
+void mv_startstopper(void) { }
+
+int getbutbmou(void) { return 0; }
+
+int getbutjmou(void) { return 0; }
+
+int Billint = 0;
+
+int mk_kbhit(void) { return 0; }
+int mk_getstate(int unused) { return 0; }
+int mk_getextchar(void) { return 0; }
+
+void mk_emptychar(void) { }
+
+char* kodtobillnev(int billkod) { return nullptr; }
+void initdsound(int secondaryeloir) {}
+void di_reset_state(state_s* mut) {}
+int controlaltnyomva(void) { return 0; }
+void mv_exit(char* text) { exit(0); }

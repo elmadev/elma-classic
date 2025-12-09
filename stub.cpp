f0511ef9387b8d04cb156b024ae592bb92760a8a
Bene <bene@datakonstruktion.com>
2025-12-09 17:42:00 +0200

    chore: format all the files

--- stub.cpp
+++ stub.cpp
@@ -1,45 +1,35 @@
 #include "ALL.H"
 
-int main() {
-	return 0;
-}
+int main() { return 0; }
 
 int Locky0_alul = 0;
 
-unsigned char** lockbackbuffer(int unused) {
-	return nullptr;
-}
+unsigned char** lockbackbuffer(int unused) { return nullptr; }
 
-void unlockbackbuffer() { }
+void unlockbackbuffer() {}
 
-unsigned char** lockfrontbuffer() {
-	return nullptr;
-}
+unsigned char** lockfrontbuffer() { return nullptr; }
 
-void unlockfrontbuffer() { }
+void unlockfrontbuffer() {}
 
-ddpal::ddpal(unsigned char* tomb) { }
-ddpal::~ddpal() { }
-void ddpal::set() { }
+ddpal::ddpal(unsigned char* tomb) {}
+ddpal::~ddpal() {}
+void ddpal::set() {}
 
-void uzenet(char* text1, char* text2, char* text3) {
-	printf("%s %s %s\n", text1, text2, text3);
-}
+void uzenet(char* text1, char* text2, char* text3) { printf("%s %s %s\n", text1, text2, text3); }
 
-void hiba(char* text1, char* text2, char* text3) {
-	printf("%s %s %s\n", text1, text2, text3);
-}
+void hiba(char* text1, char* text2, char* text3) { printf("%s %s %s\n", text1, text2, text3); }
 
 int s_random(int maximum) { return 0; }
 
-void mv_check(int unused) { }
+void mv_check(int unused) {}
 
-void setmou(int x, int y) { }
-void getmou(int* x, int* y) { }
+void setmou(int x, int y) {}
+void getmou(int* x, int* y) {}
 
 double mv_stopperido(void) { return 0; }
 
-void mv_startstopper(void) { }
+void mv_startstopper(void) {}
 
 int getbutbmou(void) { return 0; }
 
@@ -51,7 +41,7 @@ int mk_kbhit(void) { return 0; }
 int mk_getstate(int unused) { return 0; }
 int mk_getextchar(void) { return 0; }
 
-void mk_emptychar(void) { }
+void mk_emptychar(void) {}
 
 char* kodtobillnev(int billkod) { return nullptr; }
 void initdsound(int secondaryeloir) {}

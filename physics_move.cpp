5c9088058e1602c799f2fb4f909ff83cb9a6d11e
GauChoob <starlight_tonight@hotmail.com>
2025-12-16 14:05:10 -0500

    translate(beallit): cleanup comments
    We also add an image schematic explaining body_boundaries

--- physics_move.cpp
+++ physics_move.cpp
@@ -1,10 +1,6 @@
 #include "ALL.H"
 
-// Tudom hogy hiba WheelDeformationLength-ot physics_init.cpp-ben 0.02-re venni, amikor peldaul
-// 0.005 is jo lenne. Hiba tovabba biztostalppont regi verziojat hasznalni
-// uj helyett, de ezekkel nem ugyanugy viselkedne motor, mint regen.
-
-// Nem engedi vonalon+egykicsi tovabb kereket!:
+// Push the wheel out from the ground so it is standing on the anchor point
 static void move_wheel_out_of_ground(rigidbody* rb, vekt2* point) {
     double length = abs(rb->r - *point);
     vekt2 n = (rb->r - *point) * (1.0 / length);
@@ -13,24 +9,25 @@ static void move_wheel_out_of_ground(rigidbody* rb, vekt2* point) {
     }
 }
 
-// static double BumpThreshold = 1.5; // Itt mar nem hallatszik szelrol levalas
+// Minimum speed loss to trigger a bump sound effect
 static double BumpThreshold = 1.5;
 
-// Igazzal ter vissza, ha talppont tenylegesen az:
+// Handle collision between a wheel and one anchor point
+// Return 1 if there is collision, or 0 if no collision
+// Delete all of the velocity towards the point and keep velocity perpendicular to the point
 static int simulate_anchor_point_collision(rigidbody* rb, vekt2* point, vekt2 force) {
+    // Return 0 if no collision
     double length = abs(rb->r - *point);
     vekt2 n = (rb->r - *point) * (1.0 / length);
     if (n * rb->v > -GroundEscapeVelocity && n * force > 0) {
-        // Nem talppont:
         return 0;
     }
-    // Sebesseg korrigalasa csak n-re meroleges komponensre:
+    // Remove the velocity component that is parallel to the axis between wheel and point
     vekt2 deleted_velocity = n * rb->v * n;
     rb->v = rb->v - deleted_velocity;
-    // Hang:
+    // Make a "bump" sound effect if enough velocity deleted
     double bump_magnitude = abs(deleted_velocity);
     if (bump_magnitude > BumpThreshold) {
-        // bump_magnitude = bump_magnitude/BumpThreshold*0.1;
         bump_magnitude = bump_magnitude / 0.8 * 0.1;
         if (bump_magnitude >= 0.99) {
             bump_magnitude = 0.99;
@@ -40,13 +37,24 @@ static int simulate_anchor_point_collision(rigidbody* rb, vekt2* point, vekt2 fo
     return 1;
 }
 
-// Igaz, ha talppont point1 (regi modszer):
+// Check to see whether the wheel is stuck
+// Assuming that the wheel hits point point2
+// Assuming that the wheel is rolling on point2, do the forces on the wheel push towards point1 or
+// away?
+// If the wheel is pushed towards point1, return 1 (stuck wheel).
+// The reason why this causes stuck in Across is we ignore existing velocity and only consider
+// acceleration/torque
 static int valid_anchor_points_old(vekt2 point1, vekt2 point2, rigidbody* rb, vekt2 force,
                                    double torque) {
+    // Get the unit vectors
     double length = abs(rb->r - point2);
     vekt2 n = (rb->r - point2) * (1.0 / length);
     vekt2 n90 = forgatas90fokkal(n);
+
+    // Convert linear force into torque and get the net torque (gas/brake torque + linear force
+    // torque)
     double total_torque = torque + length * n90 * force;
+    // Check if torque is in same direction as point1 or not
     if ((point1 - point2) * n90 * total_torque < 0) {
         return 0;
     } else {
@@ -54,13 +62,20 @@ static int valid_anchor_points_old(vekt2 point1, vekt2 point2, rigidbody* rb, ve
     }
 }
 
-// Igaz, ha talppont point1 (uj modszer):
+// Check to see whether the wheel is stuck, Elma-exclusive
+// Assuming that the wheel hits point point2
+// Assuming that the wheel is rolling on point2, does the wheel slide towards point1 or away?
 static int valid_anchor_points_new(vekt2 point1, vekt2 point2, rigidbody* rb, vekt2 force,
                                    double torque) {
+    // Get the unit vectors
     double length = abs(rb->r - point2);
     vekt2 n = (rb->r - point2) * (1.0 / length);
     vekt2 n90 = forgatas90fokkal(n);
+
+    // Get the velocity orthogonal to the point of collision + angular velocity
+    // The units here don't really match (m/s + rad/s) so it's kind of flawed
     double speed_direction = rb->angular_velocity + length * n90 * rb->v;
+    // Check if the velocity is in the same direction as point1
     if ((point1 - point2) * n90 * speed_direction < 0) {
         return 0;
     } else {
@@ -73,34 +88,18 @@ static int valid_anchor_points_new(vekt2 point1, vekt2 point2, rigidbody* rb, ve
 int Konvex = 0;
 int Konkav = 0;*/
 
-/*int Pnyomva = 0;
+/*int Pnyomva = 0;*/
 
-rigidbody buf_pk;
-vekt2 buf_F;
-double buf_M, buf_dt;*/
-
+// Handle wheel/bike movement
+// do_collision = true if solid object (i.e. wheels and not bike)
 void rigidbody_movement(rigidbody* rb, vekt2 force, double torque, double dt, int do_collision) {
-
-    /*int betoltkezd = 0;
-    if( betoltkezd ) {
-        pk = &buf_pk;
-        F = buf_F;
-        M = buf_M;
-        dt = buf_dt;
-    }
-    else {
-        buf_pk = *pk;
-        buf_F = F;
-        buf_M = M;
-        buf_dt = dt;
-    } */
-
     int anchor_point_count = 0;
     vekt2 point1, point2;
     vonal *line1 = NULL, *line2 = NULL;
+
+    // Get up to two points of collision between wheel and polygons
     if (do_collision) {
         anchor_point_count = talppontkereses(rb->r, rb->radius, &point1, &point2, &line1, &line2);
-// Egy kis ellenorzes:
 #ifdef TEST
         switch (anchor_point_count) {
         case 0:
@@ -122,6 +121,7 @@ void rigidbody_movement(rigidbody* rb, vekt2 force, double torque, double dt, in
 #endif
     }
 
+    // Move the wheel out of the ground
     if (anchor_point_count > 0) {
         move_wheel_out_of_ground(rb, &point1);
     }
@@ -137,13 +137,11 @@ void rigidbody_movement(rigidbody* rb, vekt2 force, double torque, double dt, in
             Voltosszemosas = 1;
     } */
 
-    // Megprobalja csokkenteni talppontszamok szamat ugy, hogy figyelembe
-    // veszi masik talppont koruli elfordulast is!:
-    // Uj modszer azt nezi, hogy sebesseg kiforditja-e
-    // talppontbol.
-    // Csak egy bizonyos sebesseg felett csinaljuk, hogy
-    // Allo helyzetben ne forogjon magatol kerek:
+    // If we have two points of collision, check whether the wheel is stuck in the corner
+    // When the wheel is moving fast, try to discard one of the collision points if you are moving
+    // away from it
     if (anchor_point_count == 2 && abs(rb->v) > 1.0) {
+        // Elma-exclusive bike-stuck fixes:
         if (!valid_anchor_points_new(point1, point2, rb, force, torque)) {
             anchor_point_count = 1;
             point1 = point2;
@@ -153,14 +151,10 @@ void rigidbody_movement(rigidbody* rb, vekt2 force, double torque, double dt, in
             }
         }
     }
-    // Megprobalja csokkenteni talppontszamok szamat ugy, hogy figyelembe
-    // veszi masik talppont koruli elfordulast is, de regi
-    // modszerrel. Regi modszer azt nezi, hogy erok
-    // kiforditjak-e talppontbol.
-    // De csak kis sebessegeknel csinalja, mert eloszor egy
-    // menetben megvarja amig 2 talppont lenullazza
-    // sebesseget:
+    // When the wheel is moving slow, try to discard one of the collision points if the force pushes
+    // away from it
     if (anchor_point_count == 2 && abs(rb->v) < 1.0) {
+        // Original across bike-stuck (previously always checked, regardless of velocity)
         if (!valid_anchor_points_old(point1, point2, rb, force, torque)) {
             anchor_point_count = 1;
             point1 = point2;
@@ -171,6 +165,8 @@ void rigidbody_movement(rigidbody* rb, vekt2 force, double torque, double dt, in
         }
     }
 
+    // Assuming we have only one point of collision, check to see if we actually collide with that
+    // point! Discard the point if there's no collision
     if (anchor_point_count == 2) {
         if (!simulate_anchor_point_collision(rb, &point2, force)) {
             anchor_point_count = 1;
@@ -187,44 +183,55 @@ void rigidbody_movement(rigidbody* rb, vekt2 force, double torque, double dt, in
         }
     }
 
-    // Talppontok most mar be vannak allitva!
+    // No collision, do normal physics
     if (anchor_point_count == 0) {
-        // Szabadon forog a levegoben:
+        // Angular position, velocity and acceleration
         double angular_acceleration = torque / rb->inertia;
         rb->angular_velocity += angular_acceleration * dt;
         rb->rotation += rb->angular_velocity * dt;
+        // Linear position, velocity and acceleration
         vekt2 a = force * (1.0 / rb->mass);
         rb->v = rb->v + a * dt;
         rb->r = rb->r + rb->v * dt;
         return;
     }
+    // We collide with two points, so we are stuck
     if (anchor_point_count == 2) {
-        // Kerek meg van fogva:
         rb->v = Vekt2null;
         rb->angular_velocity = 0;
-        // Voltosszemosas = 20; //kirajz-ban egy kis teglalaot rajzol ki
+        // Voltosszemosas = 20;
         return;
     }
 
-    // Egy talppont korul fordul el kerek:
+    // We collide with one point, so roll the wheel on the ground
+    // We need to roll perpendicular to the point of contact, so get the corresponding unit vector
     double length = abs(rb->r - point1);
     vekt2 n = (rb->r - point1) * (1.0 / length);
     vekt2 n90 = forgatas90fokkal(n);
+    // Take our linear velocity - since we are rolling, convert it into equivalent angular velocity
     rb->angular_velocity = rb->v * n90 * (1.0 / rb->radius);
-    torque += force * n90 * rb->radius; // M-hez hozzajon meg ero is, mivel talpponthoz
-                                        // vonatkoztatjuk!
+    torque += force * n90 * rb->radius;
+    // Calculate the moment of inertia for rolling the wheel on the ground (as opposed to rolling
+    // wheel from the center spoke)
+    // https://en.wikipedia.org/wiki/Parallel_axis_theorem
+    // Moment(radius) = Moment(0) + mass*distance*distance
     double inertia_edge = rb->inertia + rb->mass * length * length;
+    // Calculate our angular acceleration
     double angular_acceleration = torque / inertia_edge;
 
+    // Calculate angular velocity and position
     rb->angular_velocity += angular_acceleration * dt;
     rb->rotation += rb->angular_velocity * dt;
+    // Calculate linear velocity and position based on a rolling wheel
     rb->v = rb->angular_velocity * rb->radius * n90;
     rb->r = rb->r + rb->v * dt;
     return;
 }
 
+// Teleport the body to be within the specified boundaries
+// See the image file body_boundaries.png in docs/
 static void body_boundaries(motorst* mot, vekt2 i, vekt2 j) {
-    // Konverzio i, j koordinata rendszerbe:
+    // Flip the body temporarily if the bike is turned
     double body_x, body_y;
     if (mot->flipped_bike) {
         body_x = i * (mot->bike.r - mot->body_r);
@@ -235,36 +242,37 @@ static void body_boundaries(motorst* mot, vekt2 i, vekt2 j) {
     }
     vekt2 body_r(body_x, body_y);
 
-    // Ulessel hatarolja vezetot:
+    // Restrict bottom with a diagonal line
     static vekt2 LINE_POINT(-0.35, 0.13);
-    // static vekt2 LINE_SLOPE( 0.2-(-0.35), 0.22-(0.13) );
     static vekt2 LINE_SLOPE(0.14 - (-0.35), 0.36 - (0.13));
-
     static vekt2 LINE_SLOPE_ORTHO(-LINE_SLOPE.y, LINE_SLOPE.x);
     static vekt2 LINE_SLOPE_ORTHO_UNIT = egys(LINE_SLOPE_ORTHO);
-
     if ((body_r - LINE_POINT) * LINE_SLOPE_ORTHO_UNIT < 0.0) {
-        // Vezeto ules alatt van:
         double distance = (body_r - LINE_POINT) * LINE_SLOPE_ORTHO_UNIT;
         body_r = body_r - LINE_SLOPE_ORTHO_UNIT * distance;
     }
 
-    // Felulrol hatarolja vezetot:
-    static const double ELLIPSE_HEIGHT = 0.48; // 0.5
+    // Restrict top
+    static const double ELLIPSE_HEIGHT = 0.48;
     if (body_r.y > ELLIPSE_HEIGHT) {
         body_r.y = ELLIPSE_HEIGHT;
     }
-
-    // Oldalrol hatarolja vezetot:
+    // Restrict front
     if (body_r.x < -0.5) {
         body_r.x = -0.5;
     }
-
+    // Restrict back (not very important, see next restriction)
     static const double ELLIPSE_WIDTH = 0.26;
     if (body_r.x > ELLIPSE_WIDTH) {
         body_r.x = ELLIPSE_WIDTH;
     }
 
+    // Restrict back-top corner with a curved line (ellipse)
+    // Ellipse equation
+    // (x/0.26)^2 + (y/0.48)^2 = 1.0
+    // or alternatively what we use below is:
+    // (x*0.48/0.26)^2 + y^2 = 0.48^2
+    // (x*a)^2 + y^2 = r^2
     static const double ELLIPSE_R2 = ELLIPSE_HEIGHT * ELLIPSE_HEIGHT;
     static const double ELLIPSE_R = sqrt(ELLIPSE_R2);
     if (body_r.x > 0 && body_r.y > 0) {
@@ -279,10 +287,7 @@ static void body_boundaries(motorst* mot, vekt2 i, vekt2 j) {
         }
     }
 
-    // body_r = vekt2( 0.2, 0.22 );
-    // body_r = body_r + vekt2( -(0.06), (0.14) );
-
-    // Konverzio i, j koordinata rendszerbol:
+    // Unflip body if we flipped it at the start
     if (mot->flipped_bike) {
         mot->body_r = j * body_r.y - i * body_r.x + mot->bike.r;
     } else {
@@ -290,12 +295,18 @@ static void body_boundaries(motorst* mot, vekt2 i, vekt2 j) {
     }
 }
 
+// Adjust the body position
 void body_movement(motorst* mot, vekt2 gravity, vekt2 i, vekt2 j, double dt) {
+    // Teleport the body to be within acceptable bounds
     body_boundaries(mot, i, j);
 
-    // Rugoero szamitas:
+    // Calculate the relative body offset compared to neutral position
     vekt2 neutral_body_r = mot->bike.r + j * BodyDY;
     vekt2 delta_body_r = neutral_body_r - mot->body_r;
+
+    // Acting as if the body is a spring, calculate the spring force
+    // Force = Stretch*Tension
+    // where Tension is 5x the tension of the wheel spring
     double spring_length = abs(delta_body_r);
     if (spring_length < 0.0000001) {
         spring_length = 0.0000001;
@@ -304,15 +315,25 @@ void body_movement(motorst* mot, vekt2 gravity, vekt2 i, vekt2 j, double dt) {
     double force_spring_scalar = spring_length * SpringTensionCoefficient * 5.0;
     vekt2 force_spring = force_spring_unit * force_spring_scalar;
 
-    // Surlodasi ero szamitas:
-    // Kiszamoljuk motor testenek sebesseget body_r helyen:
+    // Acting as if the body is a spring, calculate the dampening force
+    // Dampening force is proportional to velocity
+    // Force = Velocity*Resistance
+    // Where Resistance is 3x the resistance of the wheel resistance
+    // It's a bit more complicated to calculate the relative velocity of the body
+    // RelativeVelocity = BodyVel - BikeVel
+    // However, the bike velocity has linear motion as well as angular motion
+    // BikeVelocity = AngularVel(body_length) - LinearVel
+    // AngularVel = radius(m)*rotational_velocity(rad/s)
     vekt2 body_length_ortho = forgatas90fokkal(mot->body_r - mot->bike.r);
     vekt2 neutral_v = body_length_ortho * mot->bike.angular_velocity + mot->bike.v;
     vekt2 relative_v = mot->body_v - neutral_v;
     vekt2 force_damping = relative_v * SpringResistanceCoefficient * 3.0;
 
+    // Calculate gravity and then get the net force
     vekt2 force_total = force_spring - force_damping + gravity * mot->bike.mass * Gravity;
-    vekt2 a = force_total * (1.0 / mot->bike.mass); // Egyenlore kerek tomegevel szamolunk
+
+    // Update velocity and position
+    vekt2 a = force_total * (1.0 / mot->bike.mass);
     mot->body_v = mot->body_v + a * dt;
     mot->body_r = mot->body_r + mot->body_v * dt;
 }

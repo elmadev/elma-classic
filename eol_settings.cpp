37561b947a55a8d6480d66bdaa5057f76a542bf0
Joey Gouly <joey.gouly@gmail.com>
2025-12-19 09:53:11 +0000

    feat: eol settings

---
+++ eol_settings.cpp
@@ -0,0 +1,3 @@
+#include "eol_settings.h"
+
+eol_settings::eol_settings() {}
---
+++ eol_settings.h
@@ -0,0 +1,10 @@
+#ifndef EOL_SETTINGS
+#define EOL_SETTINGS
+
+struct eol_settings {
+    eol_settings();
+};
+
+extern eol_settings* EolSettings;
+
+#endif

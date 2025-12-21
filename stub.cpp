68e2223c3e74220ada74aaf4ed90c83d9cb17485
GauChoob <starlight_tonight@hotmail.com>
2025-12-21 10:43:57 -0500

    refactor(main): cross-platform hiba/uzenet

--- stub.cpp
+++ stub.cpp
@@ -1,15 +1,8 @@
 #include "ALL.H"
 
-void uzenet(const char* text1, const char* text2, const char* text3) {
-    printf("%s %s %s\n", text1, text2, text3);
+void directhiba(const char* text1, const char* text2, const char* text3, const char* text4) {
+    printf("%s\n%s\n%s\n%s\n", text1, text2, text3, text4);
     mv_exit();
 }
-
-void hiba(const char* text1, const char* text2, const char* text3) {
-    printf("%s %s %s\n", text1, text2, text3);
-    mv_exit();
-}
-
-int Billint = 0;
 
 bool Margrafikus = false;

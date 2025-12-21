4dcaaf235ddc0bcd4efe0c9728b371186f9ba394
GauChoob <starlight_tonight@hotmail.com>
2025-12-21 10:54:10 -0500

    refactor(main): cross-platform directhiba

--- stub.cpp
+++ stub.cpp
@@ -1,8 +1,1 @@
 #include "ALL.H"
-
-void directhiba(const char* text1, const char* text2, const char* text3, const char* text4) {
-    printf("%s\n%s\n%s\n%s\n", text1, text2, text3, text4);
-    mv_exit();
-}
-
-bool Margrafikus = false;

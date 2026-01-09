ac725ee13eee7b2a8e5f79c3fcacf7b1c6799e58
Bene <bene@datakonstruktion.com>
2026-01-09 17:40:19 +0200

    refactor: Remove UTKOZES.H from ALL.H

--- physics_move.cpp
+++ physics_move.cpp
@@ -2,6 +2,7 @@
 #include "ALL.H"
 #include "main.h"
 #include "physics_init.h"
+#include "UTKOZES.H"
 
 // Push the wheel out from the ground so it is standing on the anchor point
 static void move_wheel_out_of_ground(rigidbody* rb, vect2* point) {

2b3288b7baf6114225564645112301c07f3d2c8d
GauChoob <starlight_tonight@hotmail.com>
2026-01-11 13:33:29 -0500

    translate(kerek): internals

--- object.cpp
+++ object.cpp
@@ -17,25 +17,25 @@ object::object(double x, double y, int typ) {
     animation = 0;
 }
 
-constexpr double Hal_eltol = -0.15 + 0.3 * (0.07 / 0.37);
+constexpr double LETTER_K_INTERSECTION_POINT = -0.15 + 0.3 * (0.07 / 0.37);
 
 void object::render(void) {
-    int szegmensszam = 16;
-    double sugar = 0.4;
+    int slices = 16;
+    double radius = 0.4;
 
-    double elfordulas = 2.0 * PI / szegmensszam;
-    for (int i = 0; i < szegmensszam; i++) {
-        double alfa1 = i * elfordulas;
-        double alfa2 = (i + 1) * elfordulas;
-        vect2 r1(sugar * sin(alfa1), sugar * cos(alfa1));
-        vect2 r2(sugar * sin(alfa2), sugar * cos(alfa2));
+    double slice_angle = 2.0 * PI / slices;
+    for (int i = 0; i < slices; i++) {
+        double angle1 = i * slice_angle;
+        double angle2 = (i + 1) * slice_angle;
+        vect2 r1(radius * sin(angle1), radius * cos(angle1));
+        vect2 r2(radius * sin(angle2), radius * cos(angle2));
         render_line(r + r1, r + r2, false);
     }
 
     // Kis jel kor kozepere:
-    double meret = 0.02;
-    render_line(r + vect2(-meret, -meret), r + vect2(meret, meret), false);
-    render_line(r + vect2(meret, -meret), r + vect2(-meret, meret), false);
+    double length = 0.02;
+    render_line(r + vect2(-length, -length), r + vect2(length, length), false);
+    render_line(r + vect2(length, -length), r + vect2(-length, length), false);
 
     // Beleirjuk betut korbe:
     if (type == T_CEL) {
@@ -63,62 +63,62 @@ void object::render(void) {
     if (type == T_HALALOS) {
         render_line(r + vect2(-0.15, 0.3), r + vect2(-0.15, -0.3), false);
         render_line(r + vect2(-0.15, 0.07), r + vect2(0.15, -0.3), false);
-        render_line(r + vect2(Hal_eltol, 0.0), r + vect2(0.15, 0.3), false);
+        render_line(r + vect2(LETTER_K_INTERSECTION_POINT, 0.0), r + vect2(0.15, 0.3), false);
         return;
     }
-    internal_error("iugiffiif");
+    internal_error("object::render illegal type");
 }
 
 object::object(FILE* h, int version) {
     if (fread(&r.x, 1, sizeof(r.x), h) != sizeof(r.x)) {
-        internal_error("Nem olvas file-bol kerek::kerek-ben!");
+        internal_error("Failed to read object from file!");
     }
     if (fread(&r.y, 1, sizeof(r.y), h) != sizeof(r.y)) {
-        internal_error("Nem olvas file-bol kerek::kerek-ben!");
+        internal_error("Failed to read object from file!");
     }
     if (fread(&type, 1, sizeof(type), h) != sizeof(type)) {
-        internal_error("Nem olvas file-bol kerek::kerek-ben!");
+        internal_error("Failed to read object from file!");
     }
 
     property = 0;
     if (version >= 9) {
         if (fread(&property, 1, 4, h) != 4) {
-            internal_error("Nem olvas file-bol kerek::kerek-ben!");
+            internal_error("Failed to read object from file!");
         }
     }
     animation = 0;
     if (version >= 11) {
         if (fread(&animation, 1, 4, h) != 4) {
-            internal_error("Nem olvas file-bol kerek::kerek-ben!");
+            internal_error("Failed to read object from file!");
         }
     }
     if (animation < 0 || animation > 8) {
-        internal_error("65767yr");
+        internal_error("object::object invalid animation");
     }
 }
 
 void object::save(FILE* h) {
     if (fwrite(&r.x, 1, sizeof(r.x), h) != sizeof(r.x)) {
-        internal_error("Nem ir file-ba kerek::save-ben!");
+        internal_error("Failed to write object to file!");
     }
     if (fwrite(&r.y, 1, sizeof(r.y), h) != sizeof(r.y)) {
-        internal_error("Nem ir file-ba kerek::save-ben!");
+        internal_error("Failed to write object to file!");
     }
     if (fwrite(&type, 1, sizeof(type), h) != sizeof(type)) {
-        internal_error("Nem ir file-ba kerek::save-ben!");
+        internal_error("Failed to write object to file!");
     }
     if (fwrite(&property, 1, 4, h) != 4) {
-        internal_error("Nem ir file-ba kerek::save-ben!");
+        internal_error("Failed to write object to file!");
     }
     if (fwrite(&animation, 1, 4, h) != 4) {
-        internal_error("Nem ir file-ba kerek::save-ben!");
+        internal_error("Failed to write object to file!");
     }
 }
 
 double object::checksum(void) {
-    double belyeg = 0;
-    belyeg += r.x;
-    belyeg += r.y;
-    belyeg += type;
-    return belyeg;
+    double sum = 0;
+    sum += r.x;
+    sum += r.y;
+    sum += type;
+    return sum;
 }

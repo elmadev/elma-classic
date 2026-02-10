0d4f4fd2a705d645aad6c3a69bd06183ad65bfd8
GauChoob <starlight_tonight@hotmail.com>
2026-02-10 10:56:37 -0500

    feat(menu_nav): create a new copy of `menu_nav_old` called `menu_nav`

--- menu_nav.cpp
+++ menu_nav.cpp
@@ -397,3 +397,315 @@ bool menu_nav_old::search_handler(int code) {
 
     return true;
 }
+
+menu_nav::menu_nav() {
+    entries_left = nullptr;
+    entries_right = nullptr;
+    length = 0;
+    selected_index = 0;
+    x_left = 200;
+    y_entries = 90;
+    dy = 33;
+    enable_esc = true;
+    title[0] = 0;
+    y_title = 30;
+    menu = nullptr;
+    search_pattern = SearchPattern::None;
+    search_skip_one = false;
+}
+
+menu_nav::~menu_nav() {
+    if (entries_left) {
+        delete[] entries_left;
+    }
+    if (entries_right) {
+        delete[] entries_right;
+    }
+    if (menu) {
+        delete menu;
+    }
+
+    entries_left = nullptr;
+    entries_right = nullptr;
+    menu = nullptr;
+    length = 0;
+    selected_index = 0;
+    x_left = 0;
+    y_entries = 0;
+    dy = 0;
+    enable_esc = false;
+}
+
+// Load the menu with data from NavEntriesLeft and, if two columns, NavEntriesRight
+void menu_nav::setup(int len, bool two_col) {
+    if (entries_left) {
+        internal_error("menu_nav::setup called twice!");
+    }
+    length = len;
+    two_columns = two_col;
+    if (length < 1 || length > NavEntriesLeftMaxLength) {
+        internal_error("menu_nav::setup length too long!");
+    }
+    if (two_columns && length > NAV_ENTRIES_RIGHT_MAX_LENGTH) {
+        internal_error("menu_nav::setup length too long (two_columns)!");
+    }
+    entries_left = new nav_entry[length];
+    if (!entries_left) {
+        internal_error("menu_nav::setup out of memory!");
+    }
+    entries_right = nullptr;
+    if (two_columns) {
+        entries_right = new nav_entry[length];
+        if (!entries_right) {
+            internal_error("menu_nav::setup out of memory!");
+        }
+    }
+    for (int i = 0; i < length; i++) {
+        if (strlen(NavEntriesLeft[i]) > NAV_ENTRY_TEXT_MAX_LENGTH) {
+            internal_error("menu_nav::setup text length too long!: ", NavEntriesLeft[i]);
+        }
+        strcpy(entries_left[i], NavEntriesLeft[i]);
+        if (two_columns) {
+            if (strlen(NavEntriesRight[i]) > NAV_ENTRY_TEXT_MAX_LENGTH) {
+                internal_error("menu_nav::setup text length too long (two_columns)!:",
+                               NavEntriesRight[i]);
+            }
+            strcpy(entries_right[i], NavEntriesRight[i]);
+        }
+    }
+}
+
+int menu_nav::calculate_visible_entries(int extra_lines_length) {
+    int max_visible_entries = (SCREEN_HEIGHT - y_entries) / dy;
+    if (max_visible_entries < 2) {
+        max_visible_entries = 2;
+    }
+    return max_visible_entries;
+}
+
+// Render menu and return selected index (or -1 if Esc)
+int menu_nav::navigate(text_line* extra_lines, int extra_lines_length, bool render_only) {
+    if (length < 1) {
+        internal_error("menu_nav::navigate invalid setup!");
+    }
+
+    search_input.clear();
+
+    // Bound current selection
+    if (selected_index > length - 1) {
+        selected_index = length - 1;
+    }
+
+    int max_visible_entries = calculate_visible_entries(extra_lines_length);
+
+    // Center current selection on the screen
+    int view_index = selected_index - max_visible_entries / 2;
+    int view_max = length - max_visible_entries;
+
+    if (menu) {
+        delete menu;
+    }
+    menu = new menu_pic(false);
+
+    empty_keypress_buffer();
+    bool rerender = true;
+    while (true) {
+        while (!render_only && has_keypress()) {
+            Keycode c = get_keypress();
+            if (search_handler(c)) {
+                view_index = selected_index - max_visible_entries / 2;
+                rerender = true;
+                break;
+            }
+            if (c == KEY_ESC && enable_esc) {
+                CtrlAltPressed = false;
+                return -1;
+            }
+            if (c == KEY_ENTER) {
+                CtrlAltPressed = is_key_down(DIK_LCONTROL) && is_key_down(DIK_LMENU);
+                F1Pressed = is_key_down(DIK_F1);
+                return selected_index;
+            }
+            if (c == KEY_UP) {
+                selected_index--;
+            }
+            if (c == KEY_PGUP) {
+                selected_index -= max_visible_entries;
+            }
+            if (c == KEY_DOWN) {
+                selected_index++;
+            }
+            if (c == KEY_PGDOWN) {
+                selected_index += max_visible_entries;
+            }
+        }
+
+        // Limit selected index to valid values
+        if (selected_index < 0) {
+            selected_index = 0;
+        }
+        if (selected_index >= length) {
+            selected_index = length - 1;
+        }
+        // Update view_index and limit to valid values
+        if (selected_index < view_index) {
+            view_index = selected_index;
+            rerender = true;
+        }
+        if (selected_index > view_index + max_visible_entries - 1) {
+            view_index = selected_index - (max_visible_entries - 1);
+            rerender = true;
+        }
+        if (view_index > view_max) {
+            view_index = view_max;
+            rerender = true;
+        }
+        if (view_index < 0) {
+            view_index = 0;
+            rerender = true;
+        }
+
+        // Rerender screen only if updated menu position
+        if (rerender) {
+            rerender = false;
+            menu->clear();
+
+            // Fixed-position extra text lines
+            for (int i = 0; i < extra_lines_length; i++) {
+                if (extra_lines[i].text.starts_with(MENU_CENTER_TEXT)) {
+                    menu->add_line_centered(
+                        extra_lines[i].text.substr(sizeof(MENU_CENTER_TEXT) - 1), extra_lines[i].x,
+                        extra_lines[i].y);
+                } else {
+                    menu->add_line(extra_lines[i].text, extra_lines[i].x, extra_lines[i].y);
+                }
+            }
+
+            // Title
+            if (!search_input.empty()) {
+                std::string search_title = title;
+                search_title.append(": ");
+                search_title.append(search_input);
+                menu->add_line_centered(search_title.c_str(), 320, y_title);
+            } else {
+                menu->add_line_centered(title, 320, y_title);
+            }
+
+            // Only the visible menu entries
+            for (int i = 0; i < max_visible_entries && i < length - view_index; i++) {
+                menu->add_line(entries_left[view_index + i], x_left, y_entries + i * dy);
+            }
+            if (two_columns) {
+                for (int i = 0; i < max_visible_entries && i < length - view_index; i++) {
+                    menu->add_line(entries_right[view_index + i], x_right, y_entries + i * dy);
+                }
+            }
+        }
+        menu->set_helmet(x_left - 30, y_entries + (selected_index - view_index) * dy);
+        menu->render();
+        if (render_only) {
+            return 0;
+        }
+    }
+}
+
+void menu_nav::render() {
+    if (!menu) {
+        internal_error("menu_nav::render !menu");
+    }
+    menu->render();
+}
+
+nav_entry* menu_nav::entry_left(int index) { return &entries_left[index]; }
+
+bool menu_nav::search_handler(int code) {
+    if (search_pattern == SearchPattern::None) {
+        return false;
+    }
+
+    if (code == KEY_BACKSPACE) {
+        if (!search_input.empty()) {
+            search_input.pop_back();
+        }
+    } else if (code == KEY_ESC) {
+        if (!search_input.empty()) {
+            search_input.clear();
+        } else {
+            return false;
+        }
+    } else if (accept_search_input() && MenuFont->has_char(code)) {
+        if (search_input.size() < MAX_FILENAME_LEN) {
+            search_input.push_back(code);
+        }
+    } else {
+        return false;
+    }
+
+    if (search_input.empty()) {
+        return true;
+    }
+
+    nav_entry* begin = entries_left;
+    nav_entry* end = entries_left + length;
+    if (search_skip_one) {
+        ++begin;
+    }
+
+    switch (search_pattern) {
+    case SearchPattern::Sorted: {
+        // Find the entry
+        nav_entry* match = std::lower_bound(
+            begin, end, search_input.c_str(),
+            [](const nav_entry& entry, const char* k) { return strcmpi(entry, k) < 0; });
+        selected_index = match - entries_left;
+
+        if (selected_index != length && selected_index > 0 &&
+            strnicmp(*match, search_input.c_str(), search_input.length()) != 0) {
+            size_t a = common_prefix_len(search_input.c_str(), entries_left[selected_index]);
+            size_t b = common_prefix_len(search_input.c_str(), entries_left[selected_index - 1]);
+            // Use the previous entry if it has a longer common prefix
+            if (b >= a) {
+                selected_index -= 1;
+            }
+        }
+
+        break;
+    }
+    case SearchPattern::Internals: {
+        // Try to jump via number input
+        int index_search = -1;
+        try {
+            index_search = std::stoi(search_input);
+        } catch (...) {
+        }
+
+        if (index_search >= 0) {
+            selected_index = index_search;
+            break;
+        }
+
+        // Try to find exact match
+        for (int i = 0; i < length; ++i) {
+            char* text = entries_left[i];
+            // Skip the number prefix
+            if (i >= 1) {
+                text += 2;
+            }
+            if (i >= 10) {
+                text++;
+            }
+
+            if (strnicmp(text, search_input.c_str(), search_input.size()) == 0) {
+                selected_index = i;
+                break;
+            }
+        }
+        break;
+    }
+    case SearchPattern::None:
+        internal_error("search_handler() SearchPattern::None reached!");
+        break;
+    }
+
+    return true;
+}
--- menu_nav.h
+++ menu_nav.h
@@ -58,4 +58,38 @@ class menu_nav_old {
 extern bool CtrlAltPressed;
 extern bool F1Pressed;
 
+class menu_nav {
+    nav_entry* entries_left;
+    nav_entry* entries_right;
+    int length;
+    bool two_columns;
+    menu_pic* menu;
+    std::string search_input;
+
+  public:
+    int selected_index;
+    int x_left;
+    int y_entries;
+    int dy;
+    int x_right;
+    int y_title;
+    bool enable_esc;
+    char title[100];
+    SearchPattern search_pattern;
+    bool search_skip_one;
+
+    menu_nav();
+    ~menu_nav();
+    void setup(int len, bool two_col = false);
+    int navigate(text_line* extra_lines = nullptr, int extra_lines_length = 0,
+                 bool render_only = false);
+
+    void render();
+    nav_entry* entry_left(int index);
+
+  private:
+    int calculate_visible_entries(int extra_lines_length);
+    bool search_handler(int code);
+};
+
 #endif

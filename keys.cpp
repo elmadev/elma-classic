1347a4bd47cdf4e172c88368fb14e60844f951af
Linus Eriksson <bene@datakonstruktion.com>
2026-02-16 15:32:35 +0200

    refactor(keys): make KeyBuffer char-only and remove old functions
    Remove `get_keypress()`, `has_keypress()`, and `empty_keypress_buffer()`.

--- keys.cpp
+++ keys.cpp
@@ -1,27 +1,22 @@
 #include "keys.h"
-#include "platform_impl.h"
 #include "platform_utils.h"
 #include <deque>
-
-constexpr int KEY_BUFFER_SIZE = 64;
-static Keycode KeyBuffer[KEY_BUFFER_SIZE];
-static int KeyBufferCount = 0;
 
 static constexpr size_t TEXT_INPUT_BUFFER_MAX_SIZE = 64;
 static std::deque<char> TextInputBuffer;
 
 void add_key_to_buffer(Keycode keycode) {
-    if (KeyBufferCount >= KEY_BUFFER_SIZE) {
+    if (!is_ascii_character(keycode)) {
         return;
     }
-    KeyBuffer[KeyBufferCount++] = keycode;
-    if (is_ascii_character(keycode) && TextInputBuffer.size() < TEXT_INPUT_BUFFER_MAX_SIZE) {
-        TextInputBuffer.push_back((char)keycode);
+    if (TextInputBuffer.size() >= TEXT_INPUT_BUFFER_MAX_SIZE) {
+        return;
     }
+    TextInputBuffer.push_back((char)keycode);
 }
 
 void add_text_to_buffer(const char* text) {
-    while (*text && KeyBufferCount < KEY_BUFFER_SIZE) {
+    while (*text && TextInputBuffer.size() < TEXT_INPUT_BUFFER_MAX_SIZE) {
         unsigned char c = *text;
 
         if (!is_ascii_character(c)) {
@@ -29,36 +24,9 @@ void add_text_to_buffer(const char* text) {
             continue;
         }
 
-        KeyBuffer[KeyBufferCount++] = c;
-        if (TextInputBuffer.size() < TEXT_INPUT_BUFFER_MAX_SIZE) {
-            TextInputBuffer.push_back((char)c);
-        }
+        TextInputBuffer.push_back((char)c);
         text++;
     }
-}
-
-Keycode get_keypress() {
-    while (true) {
-        handle_events();
-        if (KeyBufferCount > 0) {
-            Keycode c = KeyBuffer[0];
-            for (int i = 0; i < KeyBufferCount - 1; i++) {
-                KeyBuffer[i] = KeyBuffer[i + 1];
-            }
-            KeyBufferCount--;
-            return c;
-        }
-    }
-}
-
-void empty_keypress_buffer() {
-    handle_events();
-    KeyBufferCount = 0;
-}
-
-bool has_keypress() {
-    handle_events();
-    return KeyBufferCount > 0;
 }
 
 bool has_text_input() { return !TextInputBuffer.empty(); }

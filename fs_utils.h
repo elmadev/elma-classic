c7f93aa6f3c734ad50e43288815b5891ec92ad55
Bene <bene@datakonstruktion.com>
2026-03-05 02:14:26 +0200

    feat(fs_utils): add max_name_len parameter to find_first/find_next

--- fs_utils.h
+++ fs_utils.h
@@ -5,7 +5,7 @@ constexpr int MAX_FILENAME_LEN = 8;
 constexpr int MAX_FILENAME_EXT_LEN = MAX_FILENAME_LEN + 4;
 
 typedef char finame[20];
-bool find_first(const char* pattern, char* filename_dest);
+bool find_first(const char* pattern, char* filename_dest, int max_name_len = MAX_FILENAME_LEN);
 bool find_next(char* filename_dest);
 void find_close();
 

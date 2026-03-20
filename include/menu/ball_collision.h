eb469563b61d952e7f86acb706b56567a5e888de
GauChoob <starlight_tonight@hotmail.com>
2026-03-20 10:45:20 -0400

    fix(ball): prevent balls going OOB with resolution changes

--- include/menu/ball_collision.h
+++ include/menu/ball_collision.h
@@ -16,6 +16,8 @@ void simulate_ball_ball_collision(ball* ball1, ball* ball2, double time);
 double ball_wall_collision_time(ball* ball, WallId wall);
 void simulate_ball_wall_collision(ball* ball, WallId wall, double time);
 
+void clamp_ball_position(ball& ball, double time);
+
 extern bool WallsDisabled;
 
 #endif

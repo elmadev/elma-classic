a663bcd9ea67d20ff528c10aea8ffa8ecee9d2d5
GauChoob <starlight_tonight@hotmail.com>
2026-06-23 13:27:55 -0400

    feat(pacer): implement fps booster to 1000 fps

--- src/physics/pacer.cpp
+++ src/physics/pacer.cpp
@@ -12,6 +12,7 @@ namespace {
 
 bool FpsLimitEnabled = false;
 int FpsLimit = 0;
+bool FpsBoost = false;
 
 // In physics units of time
 double time = 0.0;
@@ -74,6 +75,7 @@ void reset() {
 
     FpsLimitEnabled = EolSettings->fps_limit_enabled();
     FpsLimit = EolSettings->fps_limit();
+    FpsBoost = EolSettings->fps_boost();
 }
 
 void new_frame() {
@@ -105,7 +107,10 @@ bool subframe(double* out_dt) {
     if (0.000001 > dt) {
         return false;
     }
-    dt = std::min(dt, PHYS_MAX_TIMESTEP);
+
+    const bool boost = FpsBoost && !FpsLimitEnabled;
+    const double max_timestep = boost ? MILLISECONDS_TO_PHYS_TIME : PHYS_MAX_TIMESTEP;
+    dt = std::min(dt, max_timestep);
     *out_dt = dt;
     time += dt;
     return true;

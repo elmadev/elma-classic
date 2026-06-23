7601c351c7169c34bbf628283e40102c5607f809
GauChoob <starlight_tonight@hotmail.com>
2026-06-23 13:16:22 -0400

    feat(render): show FPS limit info on screen

--- include/physics/pacer.h
+++ include/physics/pacer.h
@@ -8,6 +8,9 @@ namespace pacer {
 constexpr double MILLISECONDS_TO_PHYS_TIME = STOPWATCH_MULTIPLIER * STOPWATCH_TO_PHYS_TIME;
 constexpr double PHYS_MAX_TIMESTEP = 0.0055;
 
+// Returns string showing current and next FPS limit
+std::string format_fps_limit();
+
 // Will be updated on the next run
 void request_fps_limit(bool enabled, int limit);
 

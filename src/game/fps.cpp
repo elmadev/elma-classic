74a289fe66535f0ef665ef77201f4f8e47385f2f
GauChoob <starlight_tonight@hotmail.com>
2026-08-06 20:59:57 -0400

    feat(fps): track gpu refresh rate

--- src/game/fps.cpp
+++ src/game/fps.cpp
@@ -9,8 +9,10 @@ constexpr long long UPDATE_INTERVAL = 1000;
 
 int frame_count;
 int update_count;
+int render_count;
 double prev_fps;
 double prev_ups;
+double prev_gpu;
 long long prev_time;
 
 std::string format_value(double value) {
@@ -28,11 +30,15 @@ std::string format_fps() { return format_value(prev_fps); }
 
 std::string format_ups() { return format_value(prev_ups); }
 
+double gpu() { return prev_gpu; }
+
 void reset() {
     frame_count = 0;
     update_count = 0;
+    render_count = 0;
     prev_fps = 0.0;
     prev_ups = 0.0;
+    prev_gpu = 0.0;
     prev_time = get_milliseconds();
 }
 
@@ -48,8 +54,10 @@ static void calculate() {
     if (dt >= update_interval) {
         prev_fps = 1000.0 * frame_count / dt;
         prev_ups = 1000.0 * update_count / dt;
+        prev_gpu = 1000.0 * render_count / dt;
         frame_count = 0;
         update_count = 0;
+        render_count = 0;
         prev_time = current_time;
     }
 }
@@ -61,5 +69,7 @@ void count_fps() {
 }
 
 void count_ups() { update_count++; }
+
+void count_gpu() { render_count++; }
 
 } // namespace fps

3ad586de1df80bf95ecf8b668239db8cf5b1e1f7
Bene <bene@datakonstruktion.com>
2026-08-16 13:18:32 +0300

    fix(controls): add one wheel status toggle to menu

--- src/menu/controls.cpp
+++ src/menu/controls.cpp
@@ -182,6 +182,7 @@ static void load_function_controls(menu_nav* nav) {
     load_control(nav, FunctionKeys, "Reconnect", &State->key_reconnect);
     load_control(nav, FunctionKeys, "Disconnect", &State->key_disconnect);
     load_control(nav, FunctionKeys, "Show Last Apple Time", &State->key_toggle_last_apple_time);
+    load_control(nav, FunctionKeys, "Show One-Wheel Status", &State->key_toggle_one_wheel_status);
 }
 
 // Menu to change controls for one player

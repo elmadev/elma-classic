Ben3eeE/elma-classic2

commit d795d8a7ad83a541892416f471dce61a05700574

refactor(sprite): extract clipping_to_string()

src/editor/window.cpp
@@ -825,13 +825,7 @@ static void editor_window_select_sprite_name(char* picture_name, char* texture_n
                         default_clipping = Lgr->textures[i + view_index].default_clipping;
                     }
 
-                    strcpy(details, "U");
-                    if (default_clipping == Clipping::Ground) {
-                        strcpy(details, "G");
-                    }
-                    if (default_clipping == Clipping::Sky) {
-                        strcpy(details, "S");
-                    }
+                    strcpy(details, clipping_to_string(default_clipping));
                     EditorBlackFont->write(BufferMain, lx1 + 92, ly1 + 15 + i * dy, details);
                 }
             }
@@ -1235,18 +1229,7 @@ void editor_window_sprite_properties(sprite* spr) {
             }
             EditorBlackFont->write_centered(BufferMain, x1 + 119, y1 + 100, tmp);
 
-            if (default_clipping == Clipping::Unknown) {
-                strcpy(tmp, "-");
-            }
-            if (default_clipping == Clipping::Unclipped) {
-                strcpy(tmp, "U");
-            }
-            if (default_clipping == Clipping::Ground) {
-                strcpy(tmp, "G");
-            }
-            if (default_clipping == Clipping::Sky) {
-                strcpy(tmp, "S");
-            }
+            strcpy(tmp, clipping_to_string(default_clipping));
             EditorBlackFont->write_centered(BufferMain, x1 + 198, y1 + 100, tmp);
 
             // Actual values
@@ -1260,15 +1243,7 @@ void editor_window_sprite_properties(sprite* spr) {
 
             render_box(BufferMain, box_clipping, EditorPaletteId::WINDOW_INPUT,
                        EditorPaletteId::WINDOW_BORDER);
-            if (clipping == Clipping::Unclipped) {
-                strcpy(tmp, "U");
-            }
-            if (clipping == Clipping::Ground) {
-                strcpy(tmp, "G");
-            }
-            if (clipping == Clipping::Sky) {
-                strcpy(tmp, "S");
-            }
+            strcpy(tmp, clipping_to_string(default_clipping));
             draw_textbox_centered(BufferMain, box_clipping, EditorPaletteId::WINDOW_INPUT, tmp);
             EditorBlackFont->write_centered(BufferMain, x1 + 198, box_clipping.y2 + 14,
                                             "(U, S, G)");

EDITUJ.CPP
@@ -376,15 +376,7 @@ void draw_sprite_tooltip(const char* picture_name, const char* texture_name, con
         EditorWhiteFont->write(&TooltipBuffer, 0, 14, "PICTURE:");
         char tmp[30];
         sprintf(tmp, "%s (%d)  ", picture_name, distance);
-        if (clipping == Clipping::Unclipped) {
-            strcat(tmp, "U");
-        }
-        if (clipping == Clipping::Ground) {
-            strcat(tmp, "G");
-        }
-        if (clipping == Clipping::Sky) {
-            strcat(tmp, "S");
-        }
+        strcat(tmp, clipping_to_string(clipping));
         EditorBlackFont->write(&TooltipBuffer, 60, 14, tmp);
     }
     if (texture_name[0] || mask_name[0]) {
@@ -393,15 +385,7 @@ void draw_sprite_tooltip(const char* picture_name, const char* texture_name, con
         EditorWhiteFont->write(&TooltipBuffer, 126, 14, "MASK:");
         char tmp[30];
         sprintf(tmp, "%s (%d)  ", mask_name, distance);
-        if (clipping == Clipping::Unclipped) {
-            strcat(tmp, "U");
-        }
-        if (clipping == Clipping::Ground) {
-            strcat(tmp, "G");
-        }
-        if (clipping == Clipping::Sky) {
-            strcat(tmp, "S");
-        }
+        strcat(tmp, clipping_to_string(clipping));
         EditorBlackFont->write(&TooltipBuffer, 164, 14, tmp);
     }
 

sprite.cpp
@@ -6,6 +6,21 @@
 #include "physics_init.h"
 #include <cstring>
 
+const char* clipping_to_string(Clipping clipping) {
+    switch (clipping) {
+    case Clipping::Unknown:
+        return "-";
+    case Clipping::Unclipped:
+        return "U";
+    case Clipping::Ground:
+        return "G";
+    case Clipping::Sky:
+        return "S";
+    default:
+        internal_error("clipping_to_string unknown!");
+    }
+}
+
 sprite::sprite(double x, double y, const char* pic_name, const char* text_name,
                const char* mask_nam) {
     if (!Lgr) {

sprite.h
@@ -3,6 +3,7 @@
 
 #include "vect2.h"
 #include <cstdio>
+#include <string>
 
 constexpr int DEFAULT_SPRITE_WIREFRAME = 20;
 
@@ -14,6 +15,8 @@ enum class Clipping {
     // only used by canvas
     Transparent = 3,
 };
+
+const char* clipping_to_string(Clipping clipping);
 
 class sprite {
   public:

elmadev/elma-classic

commit d26e6aac54a991d75824a56a0567f151e2dc25a3

fix(window): sprite window should show clipping, not default clipping
Regression was introduced in commit d795d8a7ad83 ("refactor(sprite): extract
clipping_to_string()")

src/editor/window.cpp
@@ -1212,7 +1212,7 @@ void editor_window_sprite_properties(sprite* spr) {
 
             render_box(screen.pic(), box_clipping, EditorPaletteId::WINDOW_INPUT,
                        EditorPaletteId::WINDOW_BORDER);
-            strcpy(tmp, clipping_to_string(default_clipping));
+            strcpy(tmp, clipping_to_string(clipping));
             draw_textbox_centered(screen.pic(), box_clipping, EditorPaletteId::WINDOW_INPUT, tmp);
             EditorBlackFont->write_centered(screen.pic(), x1 + 198, box_clipping.y2 + 14,
                                             "(U, S, G)");
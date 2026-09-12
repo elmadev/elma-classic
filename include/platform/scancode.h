3f46525ec7509a0ae48ffb317cab2a0333f1449a
Bene <bene@datakonstruktion.com>
2026-09-12 15:06:59 +0300

    chore: fix modernize-use-using

--- include/platform/scancode.h
+++ include/platform/scancode.h
@@ -8,7 +8,7 @@
 constexpr int MaxKeycode = 256;
 
 // DIK_ Windows scancode
-typedef int DikScancode;
+using DikScancode = int;
 static_assert(sizeof(DikScancode) <= 4);
 
 constexpr std::array<DikScancode, 8> MODIFIERS{DIK_LSHIFT, DIK_RSHIFT, DIK_LCONTROL, DIK_RCONTROL,
